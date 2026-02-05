a77ca5ec05da9b1b09fb2f6feafa765ab1b253e6
aditya kumar <adityakumar583ak@gmail.com>
2026-02-05 13:06:20 +0530

    project mayaa

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+ return(
+  <div>project mayaa</div>
+ )
+
+ 
+}
+
+export default App
---
+++ server/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import env from 'dotenv';
+env.config();
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
+
+app.get('/',(req,res)=>{
+    res.send("server is running");
+})
