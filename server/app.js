e67f84932184238724cdd438fa2e9646e5309fb7
aveenash kumar <aveenashkumar68@gmail.com>
2026-02-16 14:21:08 +0530

    db connection

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import env from 'dotenv';
+import connectDB from './src/config/db.js';
 import cors from 'cors';
 env.config();
-
+connectDB();
 const app = express();
 
 //Middleware
@@ -17,7 +18,7 @@ app.get('/',(req,res)=>{
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}`);
 })
 
 
